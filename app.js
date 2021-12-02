3010e0ba90b5c7ff53eb19013b2f58f883ed5281
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-12-02 10:34:00 +0100

    set up express server

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => res.send('Rarepress Generative API'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
